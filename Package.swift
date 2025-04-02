1fc78bfa4004616039fbb562ed5e08d73401c0b1
Marnix Arnold <marnix.arnold@cubicasa.com>
2025-04-02 09:53:45 +0200

    Fixed Package.swift path (again)

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "CubiCaptureWrapper", dependencies: ["CubiCapture", "ZIPFoundation"]),
-        .binaryTarget(name: "CubiCapture", path: "CubiCapture.xcframework")
+        .binaryTarget(name: "CubiCapture", path: "./CubiCapture.xcframework")
     ]
 )
